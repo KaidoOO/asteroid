f29f1884a270a82caec29b775e81932ee39b50e8
camilo <camilo.barottin@gmail.com>
2025-09-20 15:52:27 +0200

    draw a player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import * 
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -7,12 +8,18 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+        
     while True: 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return       
         screen.fill((0, 0, 0))
-        pygame.display.flip()
+        player.draw(screen)
+        pygame.display.flip()        
+        dt = clock.tick(60)/1000  # Limit to 60 FPS and get delta time in seconds
 
 if __name__ == "__main__":
     main()
