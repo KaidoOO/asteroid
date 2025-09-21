c285788e943a49292743503665ceef5bfce97f68
camilo <camilo.barottin@gmail.com>
2025-09-21 17:08:26 +0200

    add asteroids field

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import * 
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting Asteroids!")
@@ -13,20 +15,27 @@ def main():
     
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable,)
+
     
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
-    
+    asteroid_field = AsteroidField()
         
     while True: 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return       
-        screen.fill((0, 0, 0))
-        
+                return    
+            
         updatable.update(dt)
-        for sprite in drawable:
-            sprite.draw(screen)
+   
+        screen.fill("black")
+        
+        for obj in drawable:
+            obj.draw(screen)
         
         pygame.display.flip()        
         dt = clock.tick(60)/1000  # Limit to 60 FPS and get delta time in seconds
